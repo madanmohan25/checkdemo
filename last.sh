8129908463d3d30b72f76b7af754f9d3ce9bfef7
madanmohan25 <madanmohankb@gmail.com>
2017-08-24 16:51:02 +0530

    done

--- last.sh
+++ last.sh
@@ -3,5 +3,9 @@
 yum -y check-update
 
 yum install -y epel-release
+yum install -y git
+yum install -y curl
+yum install -y build-essential
+yum install -y libssl-dev
 
 rpm -ivh https://kojipkgs.fedoraproject.org//packages/http-parser/2.7.1/3.el7/x86_64/http-parser-2.7.1-3.el7.x86_64.rpm && yum -y install nodejs
