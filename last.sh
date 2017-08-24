dac32be0440bf76130475c1a08a3a8dd2aeec132
madanmohan25 <madanmohankb@gmail.com>
2017-08-24 16:57:46 +0530

    done

--- last.sh
+++ last.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-yum -y check-update
+sudo yum -y check-update
 
-yum install -y epel-release
-yum install -y git
-yum install -y curl
-yum install -y build-essential
-yum install -y libssl-dev
+sudo yum install -y epel-release
+sudo yum install -y git
+sudo yum install -y git
+sudo yum install -y git
+sudo yum install -y curl
+sudo yum install -y build-essential
+sudo yum install -y libssl-dev
 
-rpm -ivh https://kojipkgs.fedoraproject.org//packages/http-parser/2.7.1/3.el7/x86_64/http-parser-2.7.1-3.el7.x86_64.rpm && yum -y install nodejs
+sudo rpm -ivh https://kojipkgs.fedoraproject.org//packages/http-parser/2.7.1/3.el7/x86_64/http-parser-2.7.1-3.el7.x86_64.rpm && yum -y install nodejs
